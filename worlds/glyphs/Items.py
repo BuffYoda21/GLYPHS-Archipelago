10b7cf14760e423e527572461edefa59fa803d7b
Hayden Crookham <buffyoda21@gmail.com>
2025-08-04 16:57:55 -0500

    added glyphs itempool

--- worlds/glyphs/Items.py
+++ worlds/glyphs/Items.py
@@ -13,19 +13,19 @@ from BaseClasses import Item, ItemClassification
 
 # These come from the other files in this example. If you want to see the source ctrl + click the name
 # You can also do that ctrl + click for any functions to see what they do
-from .Types import ItemData, ChapterType, APSkeletonItem, chapter_type_to_name
+from .Types import ItemData, ChapterType, GlyphsItem, chapter_type_to_name
 from .Locations import get_total_locations
 from typing import List, Dict, TYPE_CHECKING
 
 # This is just making sure nothing gets confused dw about what its doing exactly
 if TYPE_CHECKING:
-    from . import APSkeletonWorld
+    from . import GlyphsWorld
 
 # If you're curious about the -> List[Item] that is a syntax to make sure you return the correct variable type
 # In this instance we're saying we only want to return a list of items
 # You'll see a bunch of other examples of this in other functions
 # It's main purpose is to protect yourself from yourself
-def create_itempool(world: "APSkeletonWorld") -> List[Item]:
+def create_itempool(world: "GlyphsWorld") -> List[Item]:
     # This is the empty list of items. You'll add all the items in the game to this list
     itempool: List[Item] = []
 
@@ -38,7 +38,7 @@ def create_itempool(world: "APSkeletonWorld") -> List[Item]:
 
     # For this example I'll make it so there is a starting chapter
     # We loop through all the chapters in the my_chapter section
-    for chapter in ap_skeleton_chapters.keys():
+    for chapter in glyphs_chapters.keys():
         # If the starting chapter equals the chapter we're looking at skip it
         # We skip it since we dont want to add the chapter the player started with to the item pool
         print("-------------------------")
@@ -52,8 +52,8 @@ def create_itempool(world: "APSkeletonWorld") -> List[Item]:
     
     # It's up to you and how you want things organized but I like to deal with victory here
     # This creates your win item and then places it at the "location" where you win
-    victory = create_item(world, "Victory")
-    world.multiworld.get_location("Beat Final Boss", world.player).place_locked_item(victory)
+    goal = create_item(world, "Goal")
+    world.multiworld.get_location("add dynamic goal here", world.player).place_locked_item(goal)    #dont forget to change this
 
     # Then junk items are made
     # Check out the create_junk_items function for more details
@@ -62,23 +62,23 @@ def create_itempool(world: "APSkeletonWorld") -> List[Item]:
     return itempool
 
 # This is a generic function to create a singular item
-def create_item(world: "APSkeletonWorld", name: str) -> Item:
+def create_item(world: "GlyphsWorld", name: str) -> Item:
     data = item_table[name]
-    return APSkeletonItem(name, data.classification, data.ap_code, world.player)
+    return GlyphsItem(name, data.classification, data.ap_code, world.player)
 
 # Another generic function. For creating a bunch of items at once!
-def create_multiple_items(world: "APSkeletonWorld", name: str, count: int,
+def create_multiple_items(world: "GlyphsWorld", name: str, count: int,
                           item_type: ItemClassification = ItemClassification.progression) -> List[Item]:
     data = item_table[name]
     itemlist: List[Item] = []
 
     for i in range(count):
-        itemlist += [APSkeletonItem(name, item_type, data.ap_code, world.player)]
+        itemlist += [GlyphsItem(name, item_type, data.ap_code, world.player)]
 
     return itemlist
 
 # Finally, where junk items are created
-def create_junk_items(world: "APSkeletonWorld", count: int) -> List[Item]:
+def create_junk_items(world: "GlyphsWorld", count: int) -> List[Item]:
     trap_chance = world.options.TrapChance.value
     junk_pool: List[Item] = []
     junk_list: Dict[str, int] = {}
@@ -94,10 +94,14 @@ def create_junk_items(world: "APSkeletonWorld", count: int) -> List[Item]:
         # This is for traps if your randomization includes it
         # It also grabs the trap weights from the options page
         elif trap_chance > 0 and ic == ItemClassification.trap:
-            if name == "Forcefem Trap":
-                trap_list[name] = world.options.ForcefemTrapWeight.value
-            elif name == "Speed Change Trap":
-                trap_list[name] = world.options.SpeedChangeTrapWeight.value
+            if name == "sMiLE Trap":
+                trap_list[name] = world.options.sMiLETrapWeight.value
+            elif name == "John Trap":
+                trap_list[name] = world.options.JohnTrapWeight.value
+            elif name == "Spear Trap":
+                trap_list[name] = world.options.SpearTrapWeight.value
+            elif name == "Death Trap":
+                trap_list[name] = world.options.DeathTrapWeight.value
 
     # Where all the magic happens of adding the junk and traps randomly
     # AP does all the weight management so we just need to worry about how many are created
@@ -108,6 +112,8 @@ def create_junk_items(world: "APSkeletonWorld", count: int) -> List[Item]:
         else:
             junk_pool.append(world.create_item(
                 world.random.choices(list(junk_list.keys()), weights=list(junk_list.values()), k=1)[0]))
+            if junk_pool[-1].name != "HP Refill":
+                junk_weights[junk_pool[-1].name] = 0
 
     return junk_pool
 
@@ -115,53 +121,92 @@ def create_junk_items(world: "APSkeletonWorld", count: int) -> List[Item]:
 # Watch out for overlap with your item codes
 # These are just random numbers dont trust them PLEASE
 # I've seen some games that dynamically add item codes such as DOOM as well
-ap_skeleton_items = {
-    # Progression items
-    "A cute rat": ItemData(20050001, ItemClassification.progression),
-    "Estrogen": ItemData(20050002, ItemClassification.progression),
-    "Testosterone": ItemData(20050003, ItemClassification.progression),
+glyphs_items = {
+# ---Item Name------------------------ap_code-----------------------classifications---------------------------count--
+    
+    # Major Upgrades
+    "Sword":                    ItemData(1,     ItemClassification.progression + ItemClassification.useful,         1),
+    "Progressive Dash Orb":     ItemData(2,     ItemClassification.progression + ItemClassification.useful,         2),
+    "Map":                      ItemData(3,     ItemClassification.progression + ItemClassification.useful,         1),
+    "Grapple":                  ItemData(4,     ItemClassification.progression + ItemClassification.useful,         1),
+    "Parry":                    ItemData(5,     ItemClassification.progression + ItemClassification.useful,         1),
 
-    # Useful items
-    "A good friend": ItemData(20050004, ItemClassification.useful),
-    "500 cigarettes": ItemData(20050005, ItemClassification.useful),
-    "Crime Baby": ItemData(20050006, ItemClassification.useful),
+    # Collectables
+    "Silver Shard":             ItemData(6,     ItemClassification.progression_skip_balancing,                      15),
+    "Gold Shard":               ItemData(7,     ItemClassification.useful,                                          3),
+    "Smile Token":              ItemData(8,     ItemClassification.progression_skip_balancing,                      10),
+    "Rune Cube":                ItemData(9,     ItemClassification.progression,                                     3),
+    "Void Gate Shard":          ItemData(10,    ItemClassification.progression_skip_balancing,                      7),
 
-    # Victory is added here since in this organization it needs to be in the default item pool
-    "Victory": ItemData(20050007, ItemClassification.progression)
+    # Upgrades
+    "Sword Rune":               ItemData(11,    ItemClassification.useful,                                          1),
+    "Shroud":                   ItemData(12,    ItemClassification.useful,                                          1),
+    "Fast Magic":               ItemData(13,    ItemClassification.useful,                                          1),
+    "Swift Parry":              ItemData(14,    ItemClassification.useful,                                          1),
+    "Progressive Chicken Hat":  ItemData(15,    ItemClassification.useful,                                          2),
+
+    # Goal items
+    "Ending":                   ItemData(16,    ItemClassification.progression_skip_balancing,                      7),
+    "Goal":                     ItemData(17,    ItemClassification.progression_skip_balancing,                      1),
 }
 
 # I like to split up the items so that its easier to look at and since sometimes you only need to look at one specific type of list
 # An example of that is in create_itempool where I simulated having a starting chapter
-ap_skeleton_chapters = {
-    "Green Hill Zone": ItemData(20050008, ItemClassification.progression),
-    "Romania": ItemData(20050009, ItemClassification.progression),
-    "The Sewer": ItemData(20050010, ItemClassification.progression)
+glyphs_chapters = {
+    "Menu":                     ItemData(18,    ItemClassification.progression),    # using this as starting chapter to allow randomized starting spawns
+    "Region 1":                 ItemData(19,    ItemClassification.progression),
+    "Region 2":                 ItemData(20,    ItemClassification.progression),
+    "Region 3":                 ItemData(21,    ItemClassification.progression),
+    "Region 4":                 ItemData(22,    ItemClassification.progression),
+    "Smile Shop":               ItemData(23,    ItemClassification.progression),
+    "Act 1":                    ItemData(24,    ItemClassification.progression),
+    "Act 2":                    ItemData(25,    ItemClassification.progression),
+    "Act 3":                    ItemData(26,    ItemClassification.progression),
+    "Epilogue":                 ItemData(27,    ItemClassification.progression),
 }
 
 # In the way that I made items, I added a way to specify how many of an item should exist
 # That's why junk has a 0 since how many are created is in the create_junk_items
 # There is a better way of doing this but this is my jank
 junk_items = {
-    # Junk
-    "An Old Gamecube": ItemData(20050011, ItemClassification.filler, 0),
-    "Coughing Baby": ItemData(20050012, ItemClassification.filler, 0),
+    # Junk Items
+    "HP Refill":                ItemData(27,    ItemClassification.filler,                                          0),
+    "Pink Bow":                 ItemData(28,    ItemClassification.filler,                                          0),
+    "Dumb Hat":                 ItemData(29,    ItemClassification.filler,                                          0),
+    "Traffic Cone":             ItemData(30,    ItemClassification.filler,                                          0),
+    "John Hat":                 ItemData(31,    ItemClassification.filler,                                          0),
+    "Top Hat":                  ItemData(32,    ItemClassification.filler,                                          0),
+    "Fez":                      ItemData(33,    ItemClassification.filler,                                          0),
+    "Party Hat":                ItemData(34,    ItemClassification.filler,                                          0),
+    "Bomb Hat":                 ItemData(35,    ItemClassification.filler,                                          0),
+    "Crown":                    ItemData(36,    ItemClassification.filler,                                          0),
 
     # Traps
-    "Forcefem Trap": ItemData(20050013, ItemClassification.trap, 0),
-    "Speed Change Trap": ItemData(20050014, ItemClassification, 0)
+    "sMiLE Trap":               ItemData(37,    ItemClassification.trap,                                            0),
+    "John Trap":                ItemData(38,    ItemClassification.trap,                                            0),
+    "Spear Trap":               ItemData(39,    ItemClassification.trap,                                            0),
+    "Death Trap":               ItemData(40,    ItemClassification.trap,                                            0),
 }
 
 # Junk weights is just how often an item will be chosen when junk is being made
 # Bigger item = more likely to show up
 junk_weights = {
-    "An Old Gamecube": 40,
-    "Coughing Baby": 20
+    "HP Refill":    50,
+    "Pink Bow":     10,
+    "Dumb Hat":     10,
+    "Traffic Cone": 10,
+    "John Hat":     10,
+    "Top Hat":      10,
+    "Fez":          10,
+    "Party Hat":    10,
+    "Bomb Hat":     10,
+    "Crown":        10,
 }
 
 # This makes a really convenient list of all the other dictionaries
 # (fun fact: {} is a dictionary)
 item_table = {
-    **ap_skeleton_items,
-    **ap_skeleton_chapters,
+    **glyphs_items,
+    **glyphs_chapters,
     **junk_items
 }
