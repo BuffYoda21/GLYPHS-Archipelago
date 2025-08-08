d7729408ad2577f56e10e32d70eef57ec025e2cf
BuffYoda21 <buffyoda21@gmail.com>
2025-08-08 16:34:30 -0500

    divided up glyphs regions more

--- worlds/glyphs/Regions.py
+++ worlds/glyphs/Regions.py
@@ -20,15 +20,34 @@ def create_regions(world: "GlyphsWorld"):
 
     # Kinda messy, might rework later
     menu = create_region(world, "Menu")
-    region1 = create_region_and_connect(world, "Region 1", "Menu -> Tomb Main Entrance", menu)
-    region2 = create_region_and_connect(world, "Region 2", "Region 1 Gate -> Region 2", region1)
-    region3 = create_region_and_connect(world, "Region 3", "Region 2 Long Fall -> Region 3", region2)
-    region4 = create_region_and_connect(world, "Region 4", "Region 2 Collapsed Tunnel -> Region 4", region2)
-    region3.connect(region4, "Region 3 Flower Passage -> Region 4")
-    collapse = create_region_and_connect(world, "Collapse", "Region 3 Wizard Boss Room -> Collapse", region3)
-    smile_shop = create_region_and_connect(world, "Smile Shop", "Region 2 Smile Shop Entrance -> Smile Shop", region2)
-    dark_region = create_region_and_connect(world, "Dark Region", "Region 2 Long Fall Hidden Passage-> Dark Region", region2)
-    the_between = create_region_and_connect(world, "The Between", "Region 2 Hole in the Wall-> The Between", region2)
+    region1ul = create_region_and_connect(world, "Region 1 - Upper Left", menu)
+    region1c = create_region_and_connect(world, "Region 1 - Central", region1ul)
+    region1l = create_region_and_connect(world, "Region 1 - Left", region1c)
+    region1ur = create_region_and_connect(world, "Region 1 - Upper Right", region1ul)
+    auto_connect(region1ur, region1c)
+    region2lf = create_region_and_connect(world, "Region 2 - Left", region1ur)
+    region2c = create_region_and_connect(world, "Region 2 - Central", region2lf)
+    region2s1 = create_region_and_connect(world, "Region 2 - Sector 1", region2c)
+    region2s2 = create_region_and_connect(world, "Region 2 - Sector 2", region2c)
+    auto_connect(region2s1, region2s2)
+    region2s4 = create_region_and_connect(world, "Region 2 - Sector 4", region2c)
+    region2s4e = create_region_and_connect(world, "Region 2 - Sector 4 End", region2s4)
+    region2sp = create_region_and_connect(world, "Region 2 - Serpent", region2c)
+    auto_connect(region2s2, region2sp)
+    region2low = create_region_and_connect(world, "Region 2 - Lower", region2sp)
+    region2sh = create_region_and_connect(world, "Region 2 - Shadow Chase", region2s2)
+    region3 = create_region_and_connect(world, "Region 3", region2low)
+    region4e = create_region_and_connect(world, "Region 4 - Entrance", region2c)
+    auto_connect(region4e, region2s4)
+    region4u = create_region_and_connect(world, "Region 4 - Upper", region4e)
+    region4c = create_region_and_connect(world, "Region 4 - Central", region4u)
+    region4l = create_region_and_connect(world, "Region 4 - Lower", region4c)
+    auto_connect(region3, region4l)
+    auto_connect(region4l, region2s4e)
+    collapse = create_region_and_connect(world, "Collapse", region3)
+    smile_shop = create_region_and_connect(world, "Smile Shop", region2s1)
+    dark_region = create_region_and_connect(world, "Dark Region", region2low)
+    the_between = create_region_and_connect(world, "The Between", region2lf)
     act1 = create_region_and_connect(world, "Act 1", "Menu -> Act 1", menu)
     act2 = create_region_and_connect(world, "Act 2", "Act 1 -> Act 2", act1)
     menu.connect(act2, "Menu -> Act 2")
@@ -53,8 +72,10 @@ def create_region(world: "GlyphsWorld", name: str) -> Region:
 
 # This runs the create region function while also connecting to another region
 # Just simplifies process since you woill be connecting a lot of regions
-def create_region_and_connect(world: "GlyphsWorld",
-                               name: str, entrancename: str, connected_region: Region) -> Region:
+def create_region_and_connect(world: "GlyphsWorld", name: str, connected_region: Region) -> Region:
     reg: Region = create_region(world, name)
-    connected_region.connect(reg, entrancename)
+    connected_region.connect(reg, connected_region.name + " -> " + name)
     return reg
+
+def auto_connect(region_from: Region, region_to: Region) -> None:
+    region_from.connect(region_to, region_from.name + " -> " + region_to.name)
