cc0749edf77435448f8201130de479b8a840f8b2
Hayden Crookham <buffyoda21@gmail.com>
2025-08-14 15:56:48 -0500

    removed excessive tutorial comments from glyphs

--- worlds/glyphs/Items.py
+++ worlds/glyphs/Items.py
@@ -1,72 +1,35 @@
-# So the goal here is to have a catalog of all the items in your game
-# To correctly generate a games items they need to be bundled in a list
-# A list in programming terms is anything in square brackets [] to put it simply
-
-# When a list is described its described as a list of x where x is the type of variable within it
-# IE: ["apple", "pear", "grape"] is a list of strings (anything inside "" OR '' are considered strings)
-
-# Logging = output. How you'll figure out whats going wrong
 import logging
-
-# Built in AP imports
 from BaseClasses import Item, ItemClassification
-
-# These come from the other files in this example. If you want to see the source ctrl + click the name
-# You can also do that ctrl + click for any functions to see what they do
 from .Types import ItemData, ChapterType, GlyphsItem, chapter_type_to_name
 from .Locations import get_total_locations
 from typing import List, Dict, TYPE_CHECKING
 
-# This is just making sure nothing gets confused dw about what its doing exactly
 if TYPE_CHECKING:
     from . import GlyphsWorld
 
-# If you're curious about the -> List[Item] that is a syntax to make sure you return the correct variable type
-# In this instance we're saying we only want to return a list of items
-# You'll see a bunch of other examples of this in other functions
-# It's main purpose is to protect yourself from yourself
 def create_itempool(world: "GlyphsWorld") -> List[Item]:
-    # This is the empty list of items. You'll add all the items in the game to this list
     itempool: List[Item] = []
-
-    # In this function is where you would remove any starting items that you add in options such as starting chapter
-    # This is also the place you would add dynamic amounts of items from options
-    # I can point to Sly Cooper and the Thievious Raccoonus since I did that
-
-    # This is a good place to grab anything you need from options
     starting_chapter = chapter_type_to_name[ChapterType(world.options.StartingChapter)]
-
-    # For this example I'll make it so there is a starting chapter
-    # We loop through all the chapters in the my_chapter section
+    
     for chapter in glyphs_chapters.keys():
-        # If the starting chapter equals the chapter we're looking at skip it
-        # We skip it since we dont want to add the chapter the player started with to the item pool
         print("-------------------------")
         print(starting_chapter)
         print("-------------------------")
         if starting_chapter == chapter:
             continue
-        # Otherwise then we create an item with that name and add it to the item pool
         else:
             itempool.append(create_item(world, chapter))
     
-    # It's up to you and how you want things organized but I like to deal with victory here
-    # This creates your win item and then places it at the "location" where you win
     goal = create_item(world, "Goal")
-    world.multiworld.get_location("add dynamic goal here", world.player).place_locked_item(goal)    #dont forget to change this
-
-    # Then junk items are made
-    # Check out the create_junk_items function for more details
+    world.multiworld.get_location("add dynamic goal here", world.player).place_locked_item(goal)    # dont forget to change this
     itempool += create_junk_items(world, get_total_locations(world) - len(itempool) - 1)
 
     return itempool
 
-# This is a generic function to create a singular item
 def create_item(world: "GlyphsWorld", name: str) -> Item:
     data = item_table[name]
     return GlyphsItem(name, data.classification, data.ap_code, world.player)
 
-# Another generic function. For creating a bunch of items at once!
 def create_multiple_items(world: "GlyphsWorld", name: str, count: int,
                           item_type: ItemClassification = ItemClassification.progression) -> List[Item]:
     data = item_table[name]
@@ -77,7 +40,6 @@ def create_multiple_items(world: "GlyphsWorld", name: str, count: int,
 
     return itemlist
 
-# Finally, where junk items are created
 def create_junk_items(world: "GlyphsWorld", count: int) -> List[Item]:
     trap_chance = world.options.TrapChance.value
     junk_pool: List[Item] = []
@@ -89,15 +51,11 @@ def create_junk_items(world: "GlyphsWorld", count: int) -> List[Item]:
         "HP Refill":        100,
     }
 
-    # This grabs all the junk items and trap items
     for name in item_table.keys():
-        # Here we are getting all the junk item names and weights
         ic = item_table[name].classification
         if ic == ItemClassification.filler:
             junk_list[name] = junk_weights.get(name)
 
-        # This is for traps if your randomization includes it
-        # It also grabs the trap weights from the options page
         elif trap_chance > 0 and ic == ItemClassification.trap:
             if name == "sMiLE Trap":
                 trap_list[name] = 20
@@ -112,9 +70,6 @@ def create_junk_items(world: "GlyphsWorld", count: int) -> List[Item]:
             elif name == "Instakill Trap":
                 trap_list[name] = 5
                 
-
-    # Where all the magic happens of adding the junk and traps randomly
-    # AP does all the weight management so we just need to worry about how many are created
     for i in range(count):
         if trap_chance > 0 and world.random.randint(1, 100) <= trap_chance:
             junk_pool.append(world.create_item(
@@ -127,10 +82,6 @@ def create_junk_items(world: "GlyphsWorld", count: int) -> List[Item]:
 
     return junk_pool
 
-# Time for the fun part of listing all of the items
-# Watch out for overlap with your item codes
-# These are just random numbers dont trust them PLEASE
-# I've seen some games that dynamically add item codes such as DOOM as well
 glyphs_items = {
 # ---Item Name------------------------ap_code-----------------------classifications-------------------------------count--
     
@@ -188,8 +139,6 @@ glyphs_items = {
     "Crown":                    ItemData(43,    ItemClassification.filler,                                          1),
 }
 
-# I like to split up the items so that its easier to look at and since sometimes you only need to look at one specific type of list
-# An example of that is in create_itempool where I simulated having a starting chapter
 glyphs_chapters = {
     "Menu":                     ItemData(44,    ItemClassification.progression),    # using this as starting chapter to allow randomized starting spawns
     "Region 1 - Central":       ItemData(45,    ItemClassification.progression),
@@ -221,9 +170,6 @@ glyphs_chapters = {
     "Epilogue":                 ItemData(71,    ItemClassification.progression),
 }
 
-# In the way that I made items, I added a way to specify how many of an item should exist
-# That's why junk has a 0 since how many are created is in the create_junk_items
-# There is a better way of doing this but this is my jank
 junk_items = {
     # Junk Items
     "HP Refill":                ItemData(72,    ItemClassification.filler,                                          0),
@@ -237,8 +183,6 @@ junk_items = {
     "Enemy Trap":               ItemData(78,    ItemClassification.trap,                                            0),
 }
 
-# This makes a really convenient list of all the other dictionaries
-# (fun fact: {} is a dictionary)
 item_table = {
     **glyphs_items,
     **glyphs_chapters,
--- worlds/glyphs/Regions.py
+++ worlds/glyphs/Regions.py
@@ -6,18 +6,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from . import GlyphsWorld
 
-# This is where you will create your imaginary game world
-# IE: connect rooms and areas together
-# This is NOT where you'll add requirements for how to get to certain locations thats in Rules.py
-# This is also long and tediouos
 def create_regions(world: "GlyphsWorld"):
-    # The functions that are being used here will be located at the bottom to view
-    # The important part is that if its not a dead end and connects to another place then name it
-    # Otherwise you can just create the connection. Not that naming it is bad
-
-    # You can technically name your connections whatever you want as well
-    # You'll use those connection names in Rules.py
-
     # Kinda messy, might rework later
     menu = create_region(world, "Menu")
     region1ul = create_region_and_connect(world, "Region 1 - Upper Left", menu)
@@ -58,9 +47,6 @@ def create_regions(world: "GlyphsWorld"):
 
 def create_region(world: "GlyphsWorld", name: str) -> Region:
     reg = Region(name, world.player, world.multiworld)
-
-    # When we create the region we go through all the locations we made and check if they are in that region
-    # If they are and are valid, we attach it to the region
     for (key, data) in location_table.items():
         if data.region == name:
             if not is_valid_location(world, key):
@@ -71,8 +57,6 @@ def create_region(world: "GlyphsWorld", name: str) -> Region:
     world.multiworld.regions.append(reg)
     return reg
 
-# This runs the create region function while also connecting to another region
-# Just simplifies process since you woill be connecting a lot of regions
 def create_region_and_connect(world: "GlyphsWorld", name: str, connected_region: Region) -> Region:
     reg: Region = create_region(world, name)
     connected_region.connect(reg, connected_region.name + " -> " + name)
