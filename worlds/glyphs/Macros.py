cf56049781fe73d52b15cfa5aeff340fa061958c
BuffYoda21 <buffyoda21@gmail.com>
2026-07-09 17:57:51 -0500

    GLYPHS: implemented button color rando logic

--- worlds/glyphs/Macros.py
+++ worlds/glyphs/Macros.py
@@ -1,6 +1,8 @@
 from BaseClasses import CollectionState
 from typing import TYPE_CHECKING
 from .Options import GlyphsOptions
+from .Buttons import get_button_color
+from .Types import ButtonColor
 
 options: GlyphsOptions
 
@@ -23,6 +25,26 @@ def can_wall_jump(state: CollectionState, player: int, world: "GlyphsWorld") -> 
 # def can_chain_wall_jumps(state: CollectionState, player: int, world: "GlyphsWorld") -> bool:
 #     return can_wall_jump(state, player, world) and world.options.LogicalWallJumpChains.value
 
+def can_press_buttons(state: CollectionState, player: int, world: "GlyphsWorld", button_list: list[str]) -> bool:
+    for button in button_list:
+        if not can_press_button(state, player, world, button):
+            return False
+    return True
+
+def can_press_button(state: CollectionState, player: int, world: "GlyphsWorld", button: str) -> bool:
+    color = get_button_color(world, button)
+    if color == ButtonColor.RED or color == ButtonColor.BLACK:
+        return True
+    if color == ButtonColor.BLUE:
+        return can_dash(state, player)
+    if color == ButtonColor.GREEN:
+        return can_press_green_buttons(state, player)
+    if color == ButtonColor.YELLOW:
+        return can_dash_attack(state, player)
+    if color == ButtonColor.PINK:
+        return can_parry(state, player)
+    return True
+
 def can_press_green_buttons(state: CollectionState, player: int) -> bool:
     return state.has("Progressive Sword", player, 1) or state.has("Progressive Dash Orb", player, 2)
 
@@ -38,14 +60,14 @@ def has_grapple(state: CollectionState, player: int) -> bool:
 def can_parry(state: CollectionState, player: int) -> bool:
     return state.has("Progressive Parry", player, 1)
 
-def serpent_door_open(state: CollectionState, player: int) -> bool:
-    return state.has("Serpent Lock 1", player) and state.has("Serpent Lock 2", player) and state.has("Serpent Lock 3", player)
+def serpent_door_open(state: CollectionState, player: int, world: "GlyphsWorld") -> bool:
+    return state.has("Serpent Lock 1", player) and state.has("Serpent Lock 2", player) and state.has("Serpent Lock 3", player) and can_press_button(state, player, world, "R2B Gate Left")
 
 def stalker_sigils_present(state: CollectionState, player: int) -> bool:
     return state.has("False Ending", player)
 
-def shadow_chase_open(state: CollectionState, player: int) -> bool:
-    return state.has("Stalker Sigil 1", player) and state.has("Stalker Sigil 2", player) and state.has("Stalker Sigil 3", player)
+def shadow_chase_open(state: CollectionState, player: int, world: "GlyphsWorld") -> bool:
+    return state.has("Stalker Sigil 1", player) and state.has("Stalker Sigil 2", player) and state.has("Stalker Sigil 3", player) and can_press_button(state, player, world, "R2J Gate Left")
 
 def has_clarity(state: CollectionState, player: int) -> bool:
     return state.has("Clarity", player)
@@ -118,11 +140,28 @@ def act_3_available(state: CollectionState, player: int) -> bool:
 def void_gate_open(state: CollectionState, player: int) -> bool:
     return state.has("Void Gate Shard", player, 7)
 
+# depricated
 def can_start_flower_puzzle(state: CollectionState, player: int) -> bool:
     return state.can_reach_region("Region 1E", player) and can_dash(state, player)
 
+# depricated
 def can_solve_flower_puzzle(state: CollectionState, player: int) -> bool:
     return can_start_flower_puzzle(state, player) and defeated_gilded_serpent(state, player) and can_dash(state, player) and can_press_green_buttons(state, player) and has_grapple(state, player)
+
+def flower_puzzle_completion(state: CollectionState, player: int, world: "GlyphsWorld") -> int:
+    completion = 0
+    wall_jump = can_wall_jump(state, player, world)
+    if state.can_reach_region("Region 1E", player) and can_dash(state, player) and can_press_buttons(state, player, world, ["R1B 4th Lowest", "R1B 5th Lowest"]) and (wall_jump or can_press_button(state, player, world, "R1B 6th Lowest")):
+        completion += 1
+    else:
+        return completion
+    if can_press_buttons(state, player, world, ["R1B Map Room", "R2A Gate Left", "R2A Upper", "R2G Lower", "R2G Middle", "R2G Moving Platform", "R2N Chase 1"]) and (wall_jump or can_press_button(state, player, world, "R1F Right")) and (can_parry(state, player) or (get_button_color(world, "R2G Upper Middle") != ButtonColor.BLACK and can_press_buttons(state, player, world, ["R2G Upper Left", "R2G Upper Middle"]))):
+        completion += 1
+    else:
+        return completion
+    if defeated_gilded_serpent(state, player) and has_grapple(state, player) and can_press_button(state, player, world, "R2P Left"):
+        completion += 1
+    return completion
 
 def can_access_all_silver_shards(state: CollectionState, player: int) -> bool:
     return (
